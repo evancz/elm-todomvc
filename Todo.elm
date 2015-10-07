8759ea45af70f4c00c5dafa4c3f2f79b55a8fa0f
Richard Feldman <richard.t.feldman@gmail.com>
2015-10-06 22:14:13 -0700

    Replace <- with =

--- Todo.elm
+++ Todo.elm
@@ -89,45 +89,45 @@ update action model =
 
       Add ->
           { model |
-              uid <- model.uid + 1,
-              field <- "",
-              tasks <-
+              uid = model.uid + 1,
+              field = "",
+              tasks =
                   if String.isEmpty model.field
                     then model.tasks
                     else model.tasks ++ [newTask model.field model.uid]
           }
 
       UpdateField str ->
-          { model | field <- str }
+          { model | field = str }
 
       EditingTask id isEditing ->
-          let updateTask t = if t.id == id then { t | editing <- isEditing } else t
+          let updateTask t = if t.id == id then { t | editing = isEditing } else t
           in
-              { model | tasks <- List.map updateTask model.tasks }
+              { model | tasks = List.map updateTask model.tasks }
 
       UpdateTask id task ->
-          let updateTask t = if t.id == id then { t | description <- task } else t
+          let updateTask t = if t.id == id then { t | description = task } else t
           in
-              { model | tasks <- List.map updateTask model.tasks }
+              { model | tasks = List.map updateTask model.tasks }
 
       Delete id ->
-          { model | tasks <- List.filter (\t -> t.id /= id) model.tasks }
+          { model | tasks = List.filter (\t -> t.id /= id) model.tasks }
 
       DeleteComplete ->
-          { model | tasks <- List.filter (not << .completed) model.tasks }
+          { model | tasks = List.filter (not << .completed) model.tasks }
 
       Check id isCompleted ->
-          let updateTask t = if t.id == id then { t | completed <- isCompleted } else t
+          let updateTask t = if t.id == id then { t | completed = isCompleted } else t
           in
-              { model | tasks <- List.map updateTask model.tasks }
+              { model | tasks = List.map updateTask model.tasks }
 
       CheckAll isCompleted ->
-          let updateTask t = { t | completed <- isCompleted }
+          let updateTask t = { t | completed = isCompleted }
           in
-              { model | tasks <- List.map updateTask model.tasks }
+              { model | tasks = List.map updateTask model.tasks }
 
       ChangeVisibility visibility ->
-          { model | visibility <- visibility }
+          { model | visibility = visibility }
 
 
 ---- VIEW ----
